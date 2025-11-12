aecc961fef2b7f882b0b88bbafecfdf9651fcc99
abdalrahman32008-Alpa <abdalrahman32008@gmail.com>
2025-11-12 18:10:29 +0000

    feat: Add Google Analytics 4 (GA4)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactGA4 from 'react-ga4'
 import './index.css'
 import App from './App.tsx'
 import Login from './pages/Login.tsx'
 import Signup from './pages/Signup.tsx'
 import Dashboard from './pages/Dashboard.tsx'
 import AiCoach from './pages/AiCoach.tsx'
+
+// Initialize Google Analytics 4
+const gaMeasurementId = import.meta.env.VITE_GA_MEASUREMENT_ID
+if (gaMeasurementId) {
+  ReactGA4.initialize(gaMeasurementId)
+}
 
 const router = createBrowserRouter([
   {
